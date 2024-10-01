ea5d7ffe1d872211cda5dbbc85d706dbccbded67
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-01 22:37:02 +0200

    Update content and fix content

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="space-y-6">
+    <h1 class="font-bold tracking-tight text-gray-800 dark:text-gray-100">
+      Développeur Web Fullstack
+    </h1>
     <img src="/avatar.webp" alt="Kevin De Benedetti"
          class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
     >
     <h2 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
-      Salut !
+      Bienvenue sur mon portfolio !
     </h2>
     <p class="text-gray-900 dark:text-gray-400">
-      Je suis Kevin, spécialisé dans la réalisation d'applications et de sites web utilisant Javascript, Vue, Nuxt, Express & Symfony.
+      Développeur fullstack passionné, j’utilise principalement PHP, Python, et JavaScript pour créer des applications performantes et évolutives. Fasciné par le web et les nouvelles technologies, je suis toujours en quête de nouvelles façons d’améliorer mes compétences et d’optimiser mes projets. 
+      <br>
+      <br>
+      J’attache une grande importance à l’automatisation avec des outils comme GitHub Actions, Docker, et des pipelines CI/CD, garantissant des déploiements rapides et fiables pour chaque projet.
     </p>
-    <h1 class="font-bold tracking-tight text-gray-800 dark:text-gray-100">
-      Développeur Web Fullstack
-    </h1>
+    
   </div>
 </template>
 
