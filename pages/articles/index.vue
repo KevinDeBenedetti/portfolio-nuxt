1d36d583929085c9d596f6c9e18480874c2dc5bf
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-28 11:45:49 +0100

    Add robots.txt with nuxt plugin

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,14 +1,3 @@
-<template>
-  <main class="min-h-screen">
-    <AppHeader class="mb-16" title="Articles" :description="description" />
-    <ul class="space-y-16">
-      <li v-for="(article, id) in articles" :key="id">
-        <AppArticleCard :article="article" />
-      </li>
-    </ul>
-  </main>
-</template>
-
 <script setup>
 const description =
   "Voici une partie de mes réflexions et du contenu de ma documentation sur la programmation.";
@@ -21,3 +10,14 @@ const { data: articles } = await useAsyncData("all-articles", () =>
   queryContent("/articles").sort({ published: -1 }).find()
 );
 </script>
+
+<template>
+  <main class="min-h-screen">
+    <AppHeader class="mb-16" title="Articles" :description="description" />
+    <ul class="space-y-16">
+      <li v-for="(article, id) in articles" :key="id">
+        <AppArticleCard :article="article" />
+      </li>
+    </ul>
+  </main>
+</template>
