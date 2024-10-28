782c9e1f75b1a694be074ef100184bd483ac4085
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-28 11:45:49 +0100

    Add robots.txt with nuxt plugin

--- pages/projets.vue
+++ pages/projets.vue
@@ -1,16 +1,3 @@
-<template>
-  <main class="min-h-screen">
-    <AppHeader class="mb-12" title="Réalisations" :description="description" />
-    <div class="space-y-4">
-      <AppProjectCard
-        v-for="(projet, id) in projets"
-        :key="id"
-        :project="projet"
-      />
-    </div>
-  </main>
-</template>
-
 <script setup>
 const description =
   "Voici mes réalisations, si vous voyez quelque chose qui suscite votre intérêt, consultez le code et contribuez si vous avez des idées ou suggestions.";
@@ -23,3 +10,16 @@ const { data: projets } = await useAsyncData("projets-all", () =>
   queryContent("/projets").find()
 );
 </script>
+
+<template>
+  <main class="min-h-screen">
+    <AppHeader class="mb-12" title="Réalisations" :description="description" />
+    <div class="space-y-4">
+      <AppProjectCard
+        v-for="(projet, id) in projets"
+        :key="id"
+        :project="projet"
+      />
+    </div>
+  </main>
+</template>
