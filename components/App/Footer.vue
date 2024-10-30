de48ffb1ca412f39790a8636ac0692d49123218f
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-30 21:53:24 +0100

    Add legals

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -3,6 +3,8 @@
     class="max-w-2xl mx-auto text-gray-400 dark:text-gray-600 text-sm text-center pb-8"
   >
     <br>
-    <p>© 2024 Kevin De Benedetti.</p>
+    <p>© 2024 Kevin De Benedetti</p>
+    
+    <ULink to="legals" class="mt-2 text-xs hover:text-primary">Mentions Légales</ULink>
   </footer>
 </template>
