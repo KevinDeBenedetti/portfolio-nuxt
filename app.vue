a692805a5e2503c568411cff422c826cedd79ede
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-30 21:19:42 +0100

    Add google tag & cookies

--- app.vue
+++ app.vue
@@ -1,3 +1,42 @@
+<script setup lang="ts">
+
+const {
+  cookiesEnabled,
+  cookiesEnabledIds,
+  isConsentGiven,
+  isModalActive,
+  moduleOptions,
+} = useCookieControl()
+
+// example: react to a cookie being accepted
+watch(
+  () => cookiesEnabledIds.value,
+  (current, previous) => {
+    if (
+      !previous?.includes('google-analytics') &&
+      current?.includes('google-analytics')
+    ) {
+      // cookie with id `google-analytics` got added
+      window.location.reload() // placeholder for your custom change handler
+    }
+  },
+  { deep: true },
+)
+
+// onMounted(() => {
+//   console.log(cookiesEnabled.value)
+// })
+
+// Gestion de locale Cookie avec i18n
+// const { locale } = useI18n()
+// const cookieControl = useCookieControl()
+
+// // Regarder les changements de langue et mettre à jour `cookieControl`
+// watch(locale, (newLocale) => {
+//   cookieControl.changeLocale(newLocale) // Change la langue dans `cookieControl`
+// })
+</script>
+
 <template>
   <NuxtLoadingIndicator color="#14b8a6" />
   <AppNavbar />
@@ -7,6 +46,9 @@
   </UContainer>
   <div class="h-32"></div>
   <AppFooter />
+
+  <CookieControl locale="fr" />
+
 </template>
 
 <style>
@@ -24,4 +66,5 @@
   opacity: 0;
   transform: translateY(5px);
 }
+
 </style>
