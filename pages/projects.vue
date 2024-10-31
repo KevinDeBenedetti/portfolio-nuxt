1fa87fdceb62f5e3fc71fd3cd63332e96ecc4084
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2024-10-31 22:44:23 +0100

    Add i18n - English & French

---
+++ pages/projects.vue
@@ -0,0 +1,29 @@
+<script setup>
+const { t } = useI18n()
+
+useSeoMeta({
+  title: t('projects.title'),
+  ogTitle: t('projects.title'),
+  description: t('projects.description'),
+  ogDescription: t('projects.description'),
+  ogImage: '/images/avatar.png',
+  twitterCard: 'summary_large_image',
+});
+
+const { data: projects } = await useAsyncData("projects-all", () =>
+  queryContent("/projects").find()
+);
+</script>
+
+<template>
+  <main class="min-h-screen">
+    <AppHeader class="mb-12" :title="t('projects.h1')" :description="t('projects.first_p')" />
+    <div class="space-y-4">
+      <AppProjectCard
+        v-for="(project, id) in projects"
+        :key="id"
+        :project="project"
+      />
+    </div>
+  </main>
+</template>
