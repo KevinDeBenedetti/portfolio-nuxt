c6a8de7721d67fc9945e939d59b7e3987329f9b6
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-05-02 00:05:03 +0200

    feat: :art: add multilingue projects home & realisations

--- components/App/ProjectCard.vue
+++ components/App/ProjectCard.vue
@@ -9,9 +9,23 @@ const props = defineProps({
 const config = useRuntimeConfig()
 const { $readFile, $directus } = useNuxtApp();
 
-const { data: imageMeta } = await useAsyncData('imageMeta', () => {
-  return $directus.request($readFile(props.project?.image))
-})
+
+// const { data: imageMeta } = await useAsyncData('imageMeta', () => {
+//   return $directus.request($readFile(props.project?.image))
+// })
+const imageMeta = ref(null)
+
+if (!props.project?.image) {
+  imageMeta.value = null;
+} else {
+  const { data } = await useAsyncData(
+    `imageMeta-${props.project.id}`,
+    () => $directus.request($readFile(props.project.image))
+  );
+  imageMeta.value = data.value;
+}
+console.log('imageMeta', imageMeta.value)
+
 </script>
 <template>
   <NuxtLink
@@ -22,9 +36,9 @@ const { data: imageMeta } = await useAsyncData('imageMeta', () => {
   >
     <div class="max-w-sm">
       <h3 class="text-sm font-medium group-hover:text-primary-600">
-        {{ project.title }}
+        {{ project?.translations[0]?.title }}
       </h3>
-      <p class="text-gray-400 text-sm">{{ project.description }}</p>
+      <p class="text-gray-400 text-sm">{{ project?.translations[0]?.description }}</p>
     </div>
     <div
       class="flex-1 border-b border-dashed border-gray-300 dark:border-gray-800 group-hover:border-gray-700"
