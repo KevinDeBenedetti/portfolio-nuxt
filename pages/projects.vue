bcab11aa24fc3495995ed3c9f6786eca61b787c0
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-05-04 01:14:51 +0200

    feat: :art: error toaster

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-const { t, locale } = useI18n()
+const { t, locale, locales } = useI18n()
 const { $directus, $readItems } = useNuxtApp()
-const codeMap: Record<string, string> = { fr: 'fr-FR', en: 'en-US' }
-const directusLang = computed(() => codeMap[locale.value] || locale.value)
+// const codeMap: Record<string, string> = { fr: 'fr-FR', en: 'en-US' }
+// const directusLang = computed(() => codeMap[locale.value] || locale.value)
+const currentLocale = locales.value.find((l) => l.code === locale.value)
 
 const { page } = await usePageContent('projects')
 
-const { data: projects } = await useAsyncData(
-  `projects-${directusLang.value}`,
+const { data: projects, error } = await useAsyncData(
+  `projects-${currentLocale?.code}`,
   () => $directus.request($readItems('projects', {
     fields: ['*', { translations: ['*'] }],
     deep: {
-      translations: { _filter: { languages_code: { _eq: directusLang.value } } }
+      translations: { _filter: { languages_code: { _eq: currentLocale?.iso } } }
     }
   }))
 )
@@ -24,6 +25,16 @@ useSeoMeta({
   ogImage: 'https://www.kevindb.dev/images/projects.webp',
   twitterCard: 'summary_large_image',
 })
+
+const toast = useToast()
+
+if (error.value) {
+  toast.add({ 
+    title: t('error.title'),
+    description: t('error.projects'),
+    color: 'error'
+  })
+}
 </script>
 
 <template>
