3f98b03980bd1bdbd13de420f8119154948db01b
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-05-03 19:56:18 +0200

    feat: :art: Add page to directus & use page === projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 const { t, locale } = useI18n()
-const { $directus, $readItems } = useNuxtApp()
+const { $directus, $readItems, $getPage } = useNuxtApp()
 
 const codeMap: Record<string,string> = { fr: 'fr-FR', en: 'en-US' }
 const directusLang = computed(() => codeMap[locale.value] || locale.value)
 
-const { data: projects, pending, error, refresh } = await useAsyncData(
+const { data: page } = await useAsyncData(
+  `page-${directusLang.value}`,
+  () => $getPage('projects', directusLang.value)
+)
+
+const { data: projects } = await useAsyncData(
+  `projects-${directusLang.value}`,
   () => $directus.request($readItems('projects', {
     fields: ['*', { translations: ['*'] }],
     deep: {
@@ -13,16 +19,26 @@ const { data: projects, pending, error, refresh } = await useAsyncData(
     }
   })),
   { watch: [locale] }
-) 
+)
 
 useSeoMeta({
-  title: t('projects.title'),
-  ogTitle: t('projects.title'),
-  description: t('projects.description'),
-  ogDescription: t('projects.description'),
+  title: page.value?.translations[0]?.title,
+  description: page.value?.translations[0]?.description,
+  ogTitle: page.value?.translations[0]?.title,
+  ogDescription: page.value?.translations[0]?.description,
   ogImage: 'https://www.kevindb.dev/images/projects.webp',
   twitterCard: 'summary_large_image',
 })
+
+// FIXME : delete old code
+// useSeoMeta({
+//   title: t('projects.title'),
+//   ogTitle: t('projects.title'),
+//   description: t('projects.description'),
+//   ogDescription: t('projects.description'),
+//   ogImage: 'https://www.kevindb.dev/images/projects.webp',
+//   twitterCard: 'summary_large_image',
+// })
 
 // const { data: projects } = await useAsyncData("projects", () =>
 // queryCollection('projects')
@@ -33,7 +49,10 @@ useSeoMeta({
 
 <template>
   <main class="min-h-screen">
+    <AppHeader class="mb-12" :title="page?.translations[0]?.h1" :description="page?.translations[0]?.first_p" />
+    <!--
     <AppHeader class="mb-12" :title="t('projects.h1')" :description="t('projects.first_p')" />
+    -->
     <div class="space-y-4">
       <AppProjectCard
         v-for="(project, id) in projects"
