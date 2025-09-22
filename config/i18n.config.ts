458b66469d4f5b098b00887719037db6690bb1f6
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-09-22 17:22:35 +0200

    refacor: i18n with content for texts

--- config/i18n.config.ts
+++ config/i18n.config.ts
@@ -2,10 +2,11 @@ import type { NuxtConfig } from 'nuxt/config'
 
 const i18nConfig: Partial<NuxtConfig> = {
   i18n: {
-    bundle: {
-      optimizeTranslationDirective: false,
-    },
-    vueI18n: './i18n.config.ts',
+    // FIXME :
+    // bundle: {
+    //   optimizeTranslationDirective: false,
+    // },
+    // vueI18n: './i18n.config.ts',
     locales: [
       { code: 'en', name: 'English', iso: 'en-US' },
       { code: 'fr', name: 'French', iso: 'fr-FR' },
@@ -17,28 +18,28 @@ const i18nConfig: Partial<NuxtConfig> = {
       useCookie: false,
       redirectOn: 'root',
     },
-    // pages: {
-    //   index: {
-    //     fr: '/',
-    //     en: '/',
-    //   },
-    //   projects: {
-    //     fr: '/realisations',
-    //     en: '/projects',
-    //   },
-    //   articles: {
-    //     fr: '/articles',
-    //     en: '/articles',
-    //   },
-    //   'articles-slug': {
-    //     fr: '/articles/[slug]',
-    //     en: '/articles/[slug]',
-    //   },
-    //   legals: {
-    //     fr: '/mentions-legales',
-    //     en: '/legal-notice',
-    //   },
-    // },
+    pages: {
+      index: {
+        fr: '/',
+        en: '/',
+      },
+      projects: {
+        fr: '/realisations',
+        en: '/projects',
+      },
+      articles: {
+        fr: '/articles',
+        en: '/articles',
+      },
+      'articles-slug': {
+        fr: '/articles/[slug]',
+        en: '/articles/[slug]',
+      },
+      legals: {
+        fr: '/mentions-legales',
+        en: '/legal-notice',
+      },
+    },
   },
 }
 
