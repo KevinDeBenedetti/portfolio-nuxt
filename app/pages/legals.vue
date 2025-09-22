21024782e1a48c7d16e13d88b6f05a5d2a12f55f
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-09-22 22:45:38 +0200

    refactor: simplify page content handling and improve SEO metadata usage

--- app/pages/legals.vue
+++ app/pages/legals.vue
@@ -1,17 +1,14 @@
 <script setup lang="ts">
-const { locale } = useI18n()
-const { page } = await usePageContent('legals')
+const { title, description, page, body, pending } = usePageContent('legal')
 
-useSeoMeta({
-  title: page.title,
-  ogTitle: page.title,
-  description: page.description,
-  ogDescription: page.description,
+watchEffect(() => {
+  useSeoMeta({
+    title: title.value,
+    ogTitle: title.value,
+    description: description.value,
+    ogDescription: description.value,
+  })
 })
-
-const { data: legals } = await useAsyncData('legals', () =>
-  queryCollection('legals').path(`/legals/${locale.value}`).first()
-)
 </script>
 
 <template>
@@ -19,10 +16,12 @@ const { data: legals } = await useAsyncData('legals', () =>
     <div
       class="prose dark:prose-invert prose-blockquote:not-italic prose-pre:bg-gray-900 prose-img:ring-1 prose-img:ring-gray-200 dark:prose-img:ring-white/10 prose-img:rounded-lg"
     >
-      <template v-if="legals">
-        <h1>{{ page.h1 }}</h1>
-        <ContentRenderer :value="legals" />
+      <template v-if="body && !pending">
+        <ContentRenderer :value="page" />
       </template>
+      <div v-else class="flex items-center justify-center">
+        <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
     </div>
   </main>
 </template>
