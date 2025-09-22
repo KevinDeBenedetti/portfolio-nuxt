719113d7d0cdbbce9ddab69f5929b2e5d5399af4
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-09-22 22:45:16 +0200

    fix: hydratation warning

--- app/components/App/ThemeToggle.vue
+++ app/components/App/ThemeToggle.vue
@@ -6,27 +6,32 @@ const isDark = computed({
   get() {
     return colorMode.value === 'dark'
   },
-  set() {
-    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  set(_isDark) {
+    colorMode.preference = _isDark ? 'dark' : 'light'
   },
 })
 </script>
 
 <template>
-  <UTooltip
-    :text="t('toggle_theme')"
-    :ui="{ popper: { strategy: 'absolute' } }"
-  >
-    <button
-      class="relative px-3 py-4 flex items-center justify-center cursor-pointer transition hover:text-primary-500 dark:hover:text-primary-400"
-      @click="isDark = !isDark"
+  <ClientOnly v-if="!colorMode?.forced">
+    <UTooltip
+      :text="t('toggle_theme')"
+      :ui="{ popper: { strategy: 'absolute' } }"
     >
-      <Icon
-        aria-hidden="true"
-        :name="isDark ? 'solar:sun-2-outline' : 'solar:moon-outline'"
-        class="w-5 h-5"
-      />
-      <span class="sr-only">{{ t('toggle_theme') }}</span>
-    </button>
-  </UTooltip>
+      <button
+        class="relative px-3 py-4 flex items-center justify-center cursor-pointer transition hover:text-primary-500 dark:hover:text-primary-400"
+        @click="isDark = !isDark"
+      >
+        <Icon
+          aria-hidden="true"
+          :name="isDark ? 'solar:sun-2-outline' : 'solar:moon-outline'"
+          class="w-5 h-5"
+        />
+        <span class="sr-only">{{ t('toggle_theme') }}</span>
+      </button>
+      <template #fallback>
+        <div class="size-8" />
+      </template>
+    </UTooltip>
+  </ClientOnly>
 </template>
