b1e182d0a014d21cb25a51448ac1dec52203bc1a
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-09-26 19:44:26 +0200

    fix: nuxt schema

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -6,13 +6,6 @@ export default defineNuxtSchema({
       colors: {
         primary: 'teal',
       },
-      formField: field({
-        help: 'text-xs mt-1 text-gray-500 dark:text-gray-400',
-        error: 'text-xs mt-1 text-red-500 dark:text-red-400',
-        label: {
-          base: 'text-sm block font-medium text-gray-500 dark:text-gray-200',
-        },
-      }),
       button: {
         rounded:
           'rounded-md transition-transform active:scale-x-[0.98] active:scale-y-[0.99]',
